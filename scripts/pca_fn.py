5f56072bc4cc57deac78dc912267acbc20c04fde
Pierre Sempere <pierre.sempere.01@gmail.com>
2022-04-04 12:28:43 +0200

    compatibility commit

--- scripts/pca_fn.py
+++ scripts/pca_fn.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import numpy as np
-from scipy.cluster.hierarchy import dendrogram
 
 
 def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    """
+    Returns pca circles for axis_ranks passed as list of tuples in parameters.
+     Pcs is data, n_comps is needed to error check.
+    Minor clarifications and linting from
+     original function https://github.com/stenier-oc/realisez-une-analyse-de-donnees-exploratoire
+    """
     for d1, d2 in axis_ranks:  # On affiche les 3 premiers plans factoriels, donc les 6 premières composantes
         if d2 < n_comp:
 
             # initialisation de la figure
-            fig, ax = plt.subplots(figsize=(12, 12))
+            fig, ax = plt.subplots(figsize=(18, 18))
 
             # détermination des limites du graphique
             if lims is not None:
@@ -59,67 +64,3 @@ def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0,
 
             plt.title(f"Cercle des corrélations (F{d1 + 1} et F{d2 + 1})")
             plt.show(block=False)
-
-
-def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
-    for d1, d2 in axis_ranks:
-        if d2 < n_comp:
-
-            # initialisation de la figure
-            plt.figure(figsize=(7, 6))
-
-            # affichage des points
-            if illustrative_var is None:
-                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
-            else:
-                illustrative_var = np.array(illustrative_var)
-                for value in np.unique(illustrative_var):
-                    selected = np.where(illustrative_var == value)
-                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
-                plt.legend()
-
-            # affichage des labels des points
-            if labels is not None:
-                for i, (x, y) in enumerate(X_projected[:, [d1, d2]]):
-                    plt.text(x, y, labels[i], fontsize='14', ha='center', va='center')
-
-            # détermination des limites du graphique
-            boundary = np.max(np.abs(X_projected[:, [d1, d2]])) * 1.1
-            plt.xlim([-boundary, boundary])
-            plt.ylim([-boundary, boundary])
-
-            # affichage des lignes horizontales et verticales
-            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
-            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
-
-            # nom des axes, avec le pourcentage d'inertie expliqué
-            plt.xlabel(f"F{d1+1} ({round(100*pca.explained_variance_ratio_[d1],1)}%)")
-            plt.ylabel(f"F{d2+1} ({round(100*pca.explained_variance_ratio_[d2],1)}%)")
-
-            plt.title(f"Projection des individus (sur F{d1+1} et F{d2+1}")
-            plt.show(block=False)
-
-
-def display_scree_plot(pca):
-    """
-    uses PCA passed in parameter to display a scree plot
-    """
-    scree = pca.explained_variance_ratio_*100
-    plt.bar(np.arange(len(scree))+1, scree)
-    plt.plot(np.arange(len(scree))+1, scree.cumsum(), c="red", marker='o')
-    plt.xlabel("rang de l'axe d'inertie")
-    plt.ylabel("pourcentage d'inertie")
-    plt.title("Eboulis des valeurs propres")
-    plt.show(block=False)
-
-
-def plot_dendrogram(Z, names):
-    plt.figure(figsize=(10, 25))
-    plt.title('Hierarchical Clustering Dendrogram')
-    plt.xlabel('distance')
-    dendrogram(
-        Z,
-        labels=names,
-        orientation="left",
-    )
-    plt.show()
